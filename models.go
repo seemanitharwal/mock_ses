499946ce9581f428d86fd5432aa90261e49899bd
seema nitharwal <79862030+seemanitharwal@users.noreply.github.com>
2025-02-22 23:11:51 +0530

    Update models.go

--- models.go
+++ models.go
@@ -1,10 +1,10 @@
 package main
 
 type EmailRequest struct {
-	To      string `json:"to" binding:"required"`
-	From    string `json:"from" binding:"required"`
-	Subject string `json:"subject" binding:"required"`
-	Body    string `json:"body" binding:"required"`
+	Sender    string `json:"sender" binding:"required"`
+	Recipient string `json:"recipient" binding:"required"`
+	Subject   string `json:"subject" binding:"required"`
+	Body      string `json:"body" binding:"required"`
 }
 
 type EmailStats struct {
