b768b9722a23e37ee48a9ec5021287ef4618bcd6
seema nitharwal <79862030+seemanitharwal@users.noreply.github.com>
2025-02-22 23:10:59 +0530

    Update storage.go

--- storage.go
+++ storage.go
@@ -5,13 +5,6 @@ import (
 	"time"
 )
 
-type EmailStats struct {
-	TotalSent     int               `json:"total_sent"`
-	EmailsPerUser map[string]int    `json:"emails_per_user"`
-	DailyLimit    int               `json:"daily_limit"`
-	LastSentTimes map[string]string `json:"last_sent_times"`
-}
-
 var (
 	emailStats = EmailStats{
 		TotalSent:     0,
